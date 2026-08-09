63e82a0cf7811d248b9f1e64b6754478e9197582
dinok545-cmd <244297444+dinok545-cmd@users.noreply.github.com>
2026-08-09 21:15:49 +0300

    Redesign Past Simple as three-zombie arena

--- src/main.tsx
+++ src/main.tsx
@@ -5,4 +5,5 @@ import './style.css';
 import './past-game.css';
 import './pronouns-race.css';
 import './zombie-art.css';
+import './past-arena.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
