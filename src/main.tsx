1ac24da6293543a84bfdb485fc39c0884c789f24
dinok545-cmd <244297444+dinok545-cmd@users.noreply.github.com>
2026-08-09 20:34:48 +0300

    Replace Pronouns shooter with zombie chase

--- src/main.tsx
+++ src/main.tsx
@@ -3,4 +3,5 @@ import {createRoot} from 'react-dom/client';
 import {App} from './App';
 import './style.css';
 import './past-game.css';
+import './pronouns-race.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>);
